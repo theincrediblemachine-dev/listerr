b5bde47363108c70edd50f56c039e037a5e902bf
Matt Holland <mholland@appliedsystems.com>
2021-09-12 19:27:31 -0400

    added port variables

--- src/app.py
+++ src/app.py
@@ -1,10 +1,22 @@
 import requests
 import re
+import os
 import json
+from dotenv import load_dotenv
 from bs4 import BeautifulSoup
 from flask import Flask
 
+# Set Variables
+load_dotenv()
+portNumber = int(os.getenv('PORT_NUMBER',-1))
+
+# Create Flask App
 app = Flask(__name__)
+
+# Routes
+@app.route("/")
+def base():
+    return 'The Incredible Machine'
 
 @app.route("/cf-dvd-streaming-all")
 def rt():
@@ -15,4 +27,4 @@ def rt():
         return element
 
 if __name__ == "__main__":  # There is an error on this line
-    app.run(debug=True, host='0.0.0.0',port=8089)
+    app.run(debug=True, host='0.0.0.0',port=portNumber)
